abe21111316081eb0ed51bb5d4e62cabbfc0d888
Jason Koubi <jazkoubi@gmail.com>
2023-05-24 14:15:03 -0700

    cleaned

--- catkin_ws/src/perception/src/pub_object_position_ecst.py
+++ catkin_ws/src/perception/src/pub_object_position_ecst.py
@@ -6,11 +6,8 @@ from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Pose
 
 
-
 class SurfaceObjectFilter:
-
     def __init__(self, table_height_init=0.63, error_height=0.05):
-
         self._rate = rospy.Rate(5)
 
         self.table_height = table_height_init
@@ -20,30 +17,34 @@ class SurfaceObjectFilter:
         self._check_surface_ready()
 
         rospy.Subscriber(self.surface_topic, Marker, self.surface_callback)
-        self.my_pub = rospy.Publisher(
-            '/graspable_object_pose', Pose, queue_size=10)
+        self.my_pub = rospy.Publisher("/graspable_object_pose", Pose, queue_size=10)
         self.graspable_object_position = Pose()
 
-        rospy.loginfo('Ready to detect Surfaces!')
+        rospy.loginfo("Ready to detect Surfaces!")
 
     def _check_surface_ready(self):
         self._surface_data = None
         while self._surface_data is None and not rospy.is_shutdown():
             try:
                 self._surface_data = rospy.wait_for_message(
-                    self.surface_topic, Marker, timeout=1.0)
-                rospy.logdebug("Current "+self.surface_topic +
-                               " READY=>" + str(self._surface_data))
+                    self.surface_topic, Marker, timeout=1.0
+                )
+                rospy.logdebug(
+                    "Current "
+                    + self.surface_topic
+                    + " READY=>"
+                    + str(self._surface_data)
+                )
 
             except:
-                rospy.logerr("Current "+self.surface_topic +
-                             " not ready yet, retrying.")
+                rospy.logerr(
+                    "Current " + self.surface_topic + " not ready yet, retrying."
+                )
 
     def update_table_height(self, new_table_height):
         self.table_height = new_table_height
 
     def look_for_table_surface(self, z_value):
-
         delta_min = z_value - self._error_height
         delta_max = z_value + self._error_height
         is_the_table = delta_min < self.table_height < delta_max
@@ -51,7 +52,6 @@ class SurfaceObjectFilter:
         return is_the_table
 
     def surface_callback(self, msg):
-
         name = msg.ns
         self.graspable_object_position = msg.pose
         # object_pose = msg.pose
@@ -66,13 +66,12 @@ class SurfaceObjectFilter:
                     self.object_dict[name] = self.graspable_object_position
                     rospy.loginfo("Found New Surface=")
         else:
-            rospy.logdebug("Surface Object Not found "+str(name))
+            rospy.logdebug("Surface Object Not found " + str(name))
 
     def get_object_dict_detected(self):
         return self.object_dict
 
     def run(self):
-
         while not rospy.is_shutdown():
             objects_detected = self.get_object_dict_detected()
             rospy.loginfo(str(objects_detected))
@@ -80,10 +79,10 @@ class SurfaceObjectFilter:
             self._rate.sleep()
 
 
-if __name__ == '__main__':
-    rospy.init_node('object_data_extract_node', log_level=rospy.INFO)
+if __name__ == "__main__":
+    rospy.init_node("object_data_extract_node", log_level=rospy.INFO)
 
     try:
         SurfaceObjectFilter().run()
     except KeyboardInterrupt:
-        rospy.loginfo('Shutting down')
+        rospy.loginfo("Shutting down")
