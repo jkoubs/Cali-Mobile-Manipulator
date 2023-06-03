1db0b85f257ea3d6ecb9352a1bf91bd3ec533a67
Jason Koubi <jazkoubi@gmail.com>
2023-06-02 19:58:20 -0700

    cleaned manipulation script

--- catkin_ws/src/perception/src/graspable_object_pose.py
+++ catkin_ws/src/perception/src/graspable_object_pose.py
@@ -3,13 +3,13 @@
 
 import rospy
 
-from graspable_object_pose_ecst import SurfaceObjectFilter
+from graspable_object_pose_ecst import GraspableObjectPose
 
 # Here we are performing object detection for an object placed on top of a cafe table mesuring 78 cm
 if __name__ == "__main__":
     rospy.init_node("object_detection_node", log_level=rospy.INFO)
 
     try:
-        SurfaceObjectFilter(table_height_init=0.78, error_height=0.10).run()
+        GraspableObjectPose(table_height_init=0.78, error_height=0.10).run()
     except KeyboardInterrupt:
         rospy.loginfo("Shutting down")
